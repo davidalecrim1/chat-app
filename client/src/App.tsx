1bbe31388639ce01a638d6ca01901c1c85f4d994
David Alecrim <dsalecrim@outlook.com>
2025-06-18 10:56:01 -0300

    feat: initialize the client project

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  // TODOS
+  // Create the Login in Page
+  // Create the Chat Page
+  // Create the Chat Component
+  // Create the messages within the Chat
+  // See best practices for calling the websocket connection.
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
